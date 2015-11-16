29775c11e315529dc6e5f054b0cd96a9673bba35
carl01 <enjoymath@tiscali.it>
2015-11-16 02:17:25 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,39 +12,32 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings=Movie.ratings
-    # Has the user pressed Refresh button?
+    key = params[:key]
     rating_filter = params[:ratings]
-    # logger.debug(">>>CI<<< " + "in index method." + "rating filter is " + rating_filter.to_s)    
-    if rating_filter != nil
-      @movies = Movie.where(rating: rating_filter.keys).all
-      session[:rating_filter] = rating_filter
-    else
-      # Refresh not called
-      session[:first?]=true unless session[:first?]==false
-      if session[:first?]
-        # logger.debug(">>>CI<<< " + "First call.") 
-        # All checkbox enabled
-        session[:rating_filter] = Hash[@all_ratings.zip [1]*@all_ratings.length]
-        # Prepare for next invocation of index method
-        session[:first?] = false
-      else
-        # logger.debug(">>>CI<<< " + "Call after first.") 
-      end
-      # logger.debug(">>>CI<<< " + rating_filter.to_s)
-      key = params[:key]
-      if key == "title"
-        @movies = Movie.order(title: :asc)
-        @title_header_class='hilite'
-        @release_date_header_class=''
-      elsif key == "release_date"
-        @movies = Movie.order(release_date: :asc)
-        @title_header_class=''
-        @release_date_header_class='hilite'      
-      else    
-        @movies = Movie.all
-      end
+    if session[:first?] == nil
+      # All checkbox enabled
+      session[:rating_filter] = Hash[@all_ratings.zip [1]*@all_ratings.length]
+      # Prepare for next invocation of index method
+      session[:first?]=false
     end
-    # logger.debug(">>>CI<<< " + session[:rating_filter].to_s)
+    if rating_filter==nil || rating_filter.length == 0
+      rating_filter = session[:rating_filter]
+    else
+      session[:rating_filter] = rating_filter
+    end
+    logger.debug(">>>CI<<< rating_filter is " + rating_filter.to_s)
+    if key == "title"
+      # @movies = Movie.order(title: :asc)
+      @movies = Movie.where(rating: rating_filter.keys).order(title: :asc)
+      @title_header_class='hilite'
+      @release_date_header_class=''
+    elsif key == "release_date"
+      @movies = Movie.where(rating: rating_filter.keys).order(release_date: :asc)
+      @title_header_class=''
+      @release_date_header_class='hilite'      
+    else    
+      @movies = Movie.where(rating: rating_filter.keys).all
+    end
   end
 
   def new
