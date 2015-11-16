19f82cff4984e348ef90b68b4e05a94cac36191f
carl01 <enjoymath@tiscali.it>
2015-11-16 01:32:33 +0000

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,18 +11,40 @@ class MoviesController < ApplicationController
   end
 
   def index
-    key = params[:key]
-    if key == "title"
-      @movies = Movie.order(title: :asc)
-      @title_header_class='hilite'
-      @release_date_header_class=''
-    elsif key == "release_date"
-      @movies = Movie.order(release_date: :asc)
-      @title_header_class=''
-      @release_date_header_class='hilite'      
-    else    
-      @movies = Movie.all
+    @all_ratings=Movie.ratings
+    # Has the user pressed Refresh button?
+    rating_filter = params[:ratings]
+    # logger.debug(">>>CI<<< " + "in index method." + "rating filter is " + rating_filter.to_s)    
+    if rating_filter != nil
+      @movies = Movie.where(rating: rating_filter.keys).all
+      session[:rating_filter] = rating_filter
+    else
+      # Refresh not called
+      session[:first?]=true unless session[:first?]==false
+      if session[:first?]
+        # logger.debug(">>>CI<<< " + "First call.") 
+        # All checkbox enabled
+        session[:rating_filter] = Hash[@all_ratings.zip [1]*@all_ratings.length]
+        # Prepare for next invocation of index method
+        session[:first?] = false
+      else
+        logger.debug(">>>CI<<< " + "Call after first.") 
+      end
+      # logger.debug(">>>CI<<< " + rating_filter.to_s)
+      key = params[:key]
+      if key == "title"
+        @movies = Movie.order(title: :asc)
+        @title_header_class='hilite'
+        @release_date_header_class=''
+      elsif key == "release_date"
+        @movies = Movie.order(release_date: :asc)
+        @title_header_class=''
+        @release_date_header_class='hilite'      
+      else    
+        @movies = Movie.all
+      end
     end
+    # logger.debug(">>>CI<<< " + session[:rating_filter].to_s)
   end
 
   def new
